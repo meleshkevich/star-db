f1e1fd430fdcff6957a3ae3f20f8120abf63864b
IM <i.meleshkevich@gmail.com>
2020-07-19 10:41:39 +0200

    adding react app structure

---
+++ src/services/swapi-service.js
@@ -0,0 +1,52 @@
+// Async/Await for two requests - each returns Promis:
+// fetch('https://swapi.dev/api/people/1')
+//   .then( (res) =>{
+//     return res.json();
+//   })
+//   .then( (body) =>{
+//     console.log(body);
+//   });
+export default class SwapiService {   //Class-service API Swapi, for incapsulation
+  
+    _apiBase = 'https://swapi.dev/api';
+  
+    async getResource(url) {
+      const res = await fetch( `${this._apiBase}${url}`);
+  
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+    }
+  
+    return await res.json();
+     
+    }
+    async getAllPeople() {
+      const res = await this.getResource(`/people/`);
+      return res.results;
+    }
+    getPerson(id) {
+      return this.getResource(`/people/${id}`);
+    }
+    async getAllPlanets() {
+      const res = await this.getResource(`/planets/`);
+      return res.results;
+    }
+    getPlanet(id) {
+      return this.getResource(`/planets/${id}`);
+    }
+    async getAllStarships() {
+      const res = await this.getResource(`/starships/`);
+      return res.results;
+    }
+    getStarship(id) {
+      return this.getResource(`/starships/${id}`);
+    }
+  }
+  
+  const swapi = new SwapiService();
+  
+  swapi.getStarship(3).then( (p) => {
+      
+        console.log(p.name);
+     
+  });
