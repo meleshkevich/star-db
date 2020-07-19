6e708ce8344a7c3420886748881450557a9c401f
IM <i.meleshkevich@gmail.com>
2020-07-19 15:37:50 +0200

    adding API and loading indicator

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-dhmoz6vku07">
+            <div className="ldio-2d1qqs7cjji">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner; 
+
+ 
