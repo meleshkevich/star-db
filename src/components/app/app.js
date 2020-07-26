d57ffd2bf204878554d067c4bb81abc543a072c4
IM <i.meleshkevich@gmail.com>
2020-07-26 20:35:59 +0200

    adding router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,8 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
+import  { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
 export default class App extends Component {
 
   state = {
@@ -32,15 +34,20 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
+          <Router> 
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
-
             <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
+
+             <Route path= '/' 
+             render= {() => <h2>Welcome to StarDB!</h2>}
+             exact /> 
+             <Route path= '/people' component= {PeoplePage}/>
+             <Route path= '/planets' component= {PlanetsPage}/>
+             <Route path= '/starships' component= {StarshipsPage}/>
 
           </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
