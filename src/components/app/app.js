96fcc3dd25642698d56db7479486760b1a3945b9
IM <i.meleshkevich@gmail.com>
2020-07-27 21:15:58 +0200

    adding login UI

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,7 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import { PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage } from '../pages';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
@@ -17,7 +17,14 @@ import { StarshipDetails } from '../sw-components';
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn:true
+    });
   };
 
   onServiceChange = () => {
@@ -31,6 +38,7 @@ export default class App extends Component {
   };
 
   render() {
+    const { isLoggedIn } = this.state;
 
     return (
       <ErrorBoundry>
@@ -51,6 +59,17 @@ export default class App extends Component {
                       const { id } = match.params; 
                       return <StarshipDetails itemId={id}  /> 
                     }} />            
+              <Route path= '/login' 
+                render= {() => (
+                <LoginPage 
+                isLoggedIn= { isLoggedIn }
+                onLogin= { this.onLogin}/>
+                )} /> 
+              <Route path= '/secret' 
+               render= {() => (
+                <SecretPage
+                isLoggedIn= { isLoggedIn } />
+                )} />
 
           </div>
           </Router>
